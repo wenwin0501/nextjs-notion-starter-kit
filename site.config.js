0644ec8e244b15605e7ab8f5f4527f113d2f9f98
wenwin0501 <85782880+wenwin0501@users.noreply.github.com>
2021-06-12 17:06:15 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -18,7 +18,7 @@ module.exports = {
 
   // social usernames (optional)
   weibo: 'wenwin0501',
-  github: 'wenwin0501',
+  qq: 'wenwin0501',
   douban: 'wenwin0501',
 
   // default notion icon and cover images for site-wide consistency (optional)
